aba20171c697d74696d9c697c301482bb35365fb
Ayushi Agrawal <ayushi357agrawal@gmail.com>
2019-06-22 15:17:39 +0530

    Added new&create route/template

--- app.js
+++ app.js
@@ -21,8 +21,9 @@ var Blog = mongoose.model('Blog', blogSchema);
 
 app.get('/', function(req, res) {
   res.redirect('/blogs');
-})
+});
 
+// INDEX ROUTE
 app.get('/blogs', function(req, res) {
   Blog.find({}, function(err, blogs) {
     if (err) {
@@ -31,6 +32,24 @@ app.get('/blogs', function(req, res) {
       res.render('index', {blogs: blogs});
     }
   });
+});
+
+// NEW ROUTE
+app.get('/blogs/new', function(req, res) {
+  res.render('new');
+});
+
+// CREATE ROUTE
+app.post("/blogs", function(req, res) {
+    // create blog
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if(err){
+            res.render("new");
+        } else {
+            //then, redirect to the blogs
+            res.redirect("/blogs");
+        }
+    });
 });
 
 app.listen(3000, function() {
