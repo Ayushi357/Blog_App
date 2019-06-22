c4c460b88b46ba10a8d5acaf5315ede24561a250
Ayushi Agrawal <ayushi357agrawal@gmail.com>
2019-06-22 19:05:42 +0530

    Sanitized blog body, Styled Index = Blog App Completed!

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var bodyParser = require('body-parser'),
-    methodOverride = require('method-override'), mongoose = require('mongoose'),
-    express = require('express'), app = express();
+    methodOverride = require('method-override'),
+    expressSanitizer = require('express-sanitizer'),
+    mongoose = require('mongoose'), express = require('express'),
+    app = express();
 
 // APP CONFIG
 mongoose.connect(
@@ -9,6 +11,7 @@ mongoose.set('useFindAndModify', false);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride('_method'));
 
 // MONGOOSE/MODEL CONFIG
@@ -45,6 +48,7 @@ app.get('/blogs/new', function(req, res) {
 // CREATE ROUTE
 app.post('/blogs', function(req, res) {
   // create blog
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.create(req.body.blog, function(err, newBlog) {
     if (err) {
       res.render('new');
@@ -79,6 +83,7 @@ app.get('/blogs/:id/edit', function(req, res) {
 
 // UPDATE ROUTE
 app.put('/blogs/:id', function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.findByIdAndUpdate(
       req.params.id, req.body.blog, function(err, updatedBlog) {
         if (err) {
