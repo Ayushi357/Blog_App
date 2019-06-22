599d3052f4e1ac80450d482b0f6300fa8b76abbf
Ayushi Agrawal <ayushi357agrawal@gmail.com>
2019-06-22 18:31:47 +0530

    Added Destroy Route with Edit & Destroy link buttons

--- app.js
+++ app.js
@@ -89,6 +89,19 @@ app.put('/blogs/:id', function(req, res) {
       });
 });
 
+// DELETE ROUTE
+app.delete('/blogs/:id', function(req, res) {
+  // destroy blog
+  Blog.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      // redirect somewhere
+      res.redirect('/blogs');
+    }
+  });
+});
+
 app.listen(3000, function() {
   console.log('BLOG APP SERVER HAS STARTED!');
 });
